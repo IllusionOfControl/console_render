6665309806f491a970e6d2774e4b73575d569f8a
Illusion of control <sergeyskorokhod2@gmail.com>
2022-04-01 03:23:21 +0300

    render thor

--- constants.h
+++ constants.h
@@ -5,19 +5,20 @@
 
 #include <math.h>
 
-const float PI = M_PI;
+const float PI = (float) M_PI;
 
 const int SCREEN_WIDTH = 78;
 const int SCREEN_HEIGHT = 40;
 
 const int THOR_SEGMENTS = 90;
-const int CIRCLE_SEGMENTS = 90; //314
-const float THOR_RADIUS = 2.0f;
-const float CIRCLE_RADIUS = 1.0f;
+const int CIRCLE_SEGMENTS = 90;
+const float THOR_RADIUS = 5.0f;
+const float CIRCLE_RADIUS = 2.0f;
+
+const float ROTATE_X_ANGLE = 0.006f;
+const float ROTATE_Z_ANGLE = 0.004f;
 
 const float Z_RANGE = 20.f;
-const float RENDER_SCALE = 0.1f;
-
-#define VIEW_DISTANCE ((float)(SCREEN_WIDTH * Z_RANGE / THOR_RADIUS * RENDER_SCALE))
+const float RENDER_SCALE = 0.2f;
 
 #undef _USE_MATH_DEFINES
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include "constants.h"
@@ -10,7 +11,9 @@ struct RotationAngles {
 typedef struct RotationAngles RotationAngles;
 
 
-void render(RotationAngles *rot_angles, char *screenBuffer, char *lightingBuffer) {
+void render(const RotationAngles *rot_angles, char *screenBuffer, float *zBuffer) {
+    const float viewDistance = SCREEN_WIDTH * Z_RANGE / THOR_RADIUS * RENDER_SCALE;
+
     float rotSinX = sinf(rot_angles->x);
     float rotCosX = cosf(rot_angles->x);
     float rotSinZ = sinf(rot_angles->z);
@@ -23,53 +26,85 @@ void render(RotationAngles *rot_angles, char *screenBuffer, char *lightingBuffer
 
         float phiSpacing = (PI * 2) / CIRCLE_SEGMENTS;
         for (float phi = 0; phi < (PI * 2); phi += phiSpacing) {
-            float phiSin = sinf(theta);
-            float phiCos = cosf(theta);
+            float phiSin = sinf(phi);
+            float phiCos = cosf(phi);
 
-            float circleX = CIRCLE_RADIUS + THOR_RADIUS * thetaCos;
-            float circleY = THOR_RADIUS * thetaSin;
-            
-            float x = circleX;
-            float y = circleY;
-            float z = Z_RANGE;
+            float circleX = THOR_RADIUS + CIRCLE_RADIUS * thetaCos;
+            float circleY = CIRCLE_RADIUS * thetaSin;
+
+            float x = circleX * (rotCosZ * phiCos + rotSinX * rotSinZ * phiSin)
+                - circleY * rotCosX * rotSinZ;
+            float y = circleX * (rotSinZ * phiCos - rotSinX * rotCosZ * phiSin)
+                + circleY * rotCosX * rotCosZ;
+            float z = Z_RANGE + rotCosX * circleX * phiSin + circleY * rotSinX;
 
             float ooz = 1 / z;
 
-            int xp = (int)(SCREEN_WIDTH / 2 + VIEW_DISTANCE * ooz * x);
-            int yp = (int)(SCREEN_HEIGHT / 2 - VIEW_DISTANCE * ooz * y);
+            int xp = (int)(SCREEN_WIDTH / 2 + viewDistance * ooz * x);
+            int yp = (int)(SCREEN_HEIGHT / 2 - viewDistance * ooz * y);
 
-            screenBuffer[yp * SCREEN_WIDTH + xp] = '@';
+            if (xp < 0.f || xp > SCREEN_WIDTH || yp < 0.f || yp > SCREEN_HEIGHT) continue;
+
+            float light = phiCos * thetaCos * rotSinZ - rotCosX * thetaCos * phiSin -
+                rotSinX * thetaSin + rotCosZ * (rotCosX * thetaSin - thetaCos * rotSinX * phiSin);
+            
+            if (light > 0) {
+                if (ooz > zBuffer[yp * SCREEN_WIDTH + xp]) {
+                    zBuffer[yp * SCREEN_WIDTH + xp] = ooz;
+                    int luminance_index = light * 8;
+                    screenBuffer[yp * SCREEN_WIDTH + xp] = ".,-~:;=!*#$@"[luminance_index];
+                }
+            }
         }
     }
 }
 
+
 void draw(char *screenbuffer) {
     printf("\x1b[H");
-    char* line_buffer = malloc(sizeof(char) * 80);
+    char *line_buffer = (char*) malloc(sizeof(char) * 80);
+
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
         for (int x = 0; x < SCREEN_WIDTH; x++) {
             line_buffer[x] = screenbuffer[y * SCREEN_WIDTH + x];
         }
         line_buffer[78] = '\n';
         line_buffer[79] = '\0';
-        fprintf(stderr, line_buffer);
+        printf("%s", line_buffer);
     }
+    free(line_buffer);
 }
 
 
 int main() {
     const size_t bufferSize = SCREEN_WIDTH * SCREEN_HEIGHT;
     
-    RotationAngles angles = { 1.0f, 0.0f };
-    char* screenBuffer = malloc(sizeof(char) * bufferSize);
-    float* lightBuffer = malloc(sizeof(char) * bufferSize);
+    RotationAngles angles = { 0.0f, 0.0f };
+    char *screenBuffer = (char*) malloc(sizeof(char) * bufferSize);
+    float *zBuffer = (float*) malloc(sizeof(float) * bufferSize);
+
+    if (screenBuffer == NULL || zBuffer == NULL) {
+        return 1;
+    }
 
     for (;;) {
-        memset(screenBuffer, ' ', bufferSize);
-        memset(lightBuffer, 0.0f, bufferSize);
+        for (size_t i = 0; i < bufferSize; ++i) {
+            screenBuffer[i] = ' ';
+        }
+        
+        for (size_t i = 0; i < bufferSize; ++i) {
+            zBuffer[i] = 0.0f;
+        }
 
-        render(&angles, screenBuffer, lightBuffer);
+        render(&angles, screenBuffer, zBuffer);
         draw(screenBuffer);
+
+        angles.x += ROTATE_X_ANGLE;
+        angles.z += ROTATE_Z_ANGLE;
     }
+
+    free(screenBuffer);
+    free(zBuffer);
+
     return 0;
 }
