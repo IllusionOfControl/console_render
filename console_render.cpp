98032e652c08f958a07ab9306c51942ccb57ac8a
Illusion of control <sergeyskorokhod2@gmail.com>
2022-10-05 13:45:16 +0300

    console render

--- console_render.cpp
+++ console_render.cpp
@@ -4,19 +4,12 @@
 #include "objects.h"
 
 
-//void render_screen(wchar_t* screen, int tick) {
-//	
-//}
-
-
 const char GRADIENT[] = " .:!/r(l1Z4H9W8$@";
-
 
 
 int main() {
 	int width = 120 * 2;
 	int height = 30 * 2;
-	//SetWindow(width, height);
 	float aspect = (float)width / height;
 	float pixelAspect = 11.0f / 24.0f;
 	int gradientSize = std::size(GRADIENT) - 2;
@@ -28,9 +21,7 @@ int main() {
 
 	for (int t = 0; t < 10000; t++) {
 		vector3 light = norm(vector3(-0.5, 0.5, -1.0));
-		//vector3 light = norm(vector3(sin(t*0.01), cos(t * 0.01), -1.0));
 		vector3 spherePos = vector3(0, -3, 0);
-		//vector3 spherePos = vector3(0, 0, 0);
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				vector2 uv = vector2(i, j) / vector2(width, height) * 2.0f - 1.0f;
@@ -51,24 +42,23 @@ int main() {
 						n = norm(itPoint);
 					}
 					vector3 boxN = 0;
-					//intersection = box(ro, rd, 1, boxN);
 					intersection = box(ro, rd, 1, boxN);
 					if (intersection.x > 0 && intersection.x < minIt) {
 						minIt = intersection.x;
 						n = boxN;
 					}
 					vector3 box_2N = 0;
-					intersection = box(ro - {0, -4, 0}, rd, 1, box_2N);
+					intersection = box(ro - vector3(0, -6, 0), rd, 1, box_2N);
 					if (intersection.x > 0 && intersection.x < minIt) {
 						minIt = intersection.x;
 						n = box_2N;
 					}
-					//intersection = plane(ro, rd, vector3(0, 0, -1), 1);
-					//if (intersection.x > 0 && intersection.x < minIt) {
-					//	minIt = intersection.x;
-					//	n = vector3(0, 0, -1);
-					//	albedo = 0.5;
-					//}
+					intersection = plane(ro, rd, vector3(0, 0, -1), 1);
+					if (intersection.x > 0 && intersection.x < minIt) {
+						minIt = intersection.x;
+						n = vector3(0, 0, -1);
+						albedo = 0.5;
+					}
 					if (minIt < 99999) {
 						diff = (dot(n, light) * 0.5 + 0.5) * albedo;
 						ro = ro + rd * (minIt - 0.01);
@@ -87,14 +77,3 @@ int main() {
 	}
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
